10b91343436b77537e1941e2bec0da45150b1ad7
Mikhail Vyatkin <40891414+msmkv@users.noreply.github.com>
2024-05-06 21:38:00 +0300

    Update test_main.py
    Добавление комментариев для объяснения цели и действий каждого теста.
    Разделение тестов на отдельные функции для более ясной структуры.
    Использование описательных названий для тестовых функций.

--- test_main.py
+++ test_main.py
@@ -1,26 +1,34 @@
 from fastapi.testclient import TestClient
 from main import app
 
+# Создаем клиент для тестирования FastAPI приложения
 client = TestClient(app)
 
-
+# Тест для проверки корневого маршрута
 def test_read_main():
+    # Отправляем GET запрос на корневой маршрут
     response = client.get("/")
+    # Проверяем, что статус код ответа равен 200 (ОК)
     assert response.status_code == 200
+    # Проверяем, что возвращаемые данные соответствуют ожидаемому словарю
     assert response.json() == {"message": "Hello World"}
 
-
+# Тест для проверки предсказания позитивного настроения
 def test_predict_positive():
-    response = client.post("/predict/",
-                           json={"text": "I like machine learning!"})
+    # Отправляем POST запрос с текстом, который вызывает позитивное настроение
+    response = client.post("/predict/", json={"text": "I like machine learning!"})
     json_data = response.json()
+    # Проверяем, что статус код ответа равен 200 (ОК)
     assert response.status_code == 200
+    # Проверяем, что предсказанное настроение соответствует 'POSITIVE'
     assert json_data['label'] == 'POSITIVE'
 
-
+# Тест для проверки предсказания негативного настроения
 def test_predict_negative():
-    response = client.post("/predict/",
-                           json={"text": "I hate machine learning!"})
+    # Отправляем POST запрос с текстом, который вызывает негативное настроение
+    response = client.post("/predict/", json={"text": "I hate machine learning!"})
     json_data = response.json()
+    # Проверяем, что статус код ответа равен 200 (ОК)
     assert response.status_code == 200
+    # Проверяем, что предсказанное настроение соответствует 'NEGATIVE'
     assert json_data['label'] == 'NEGATIVE'
